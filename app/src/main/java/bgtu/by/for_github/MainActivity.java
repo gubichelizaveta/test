bb110e7df262dce07b636a76dcb04f25ef07ee12
gubichelizaveta <gubichelizaveta@gmail.com>
2022-09-05 09:40:05 +0300

    мб

--- app/src/main/java/bgtu/by/for_github/MainActivity.java
+++ app/src/main/java/bgtu/by/for_github/MainActivity.java
@@ -3,6 +3,10 @@ package bgtu.by.for_github;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import bgtu.by.for_github.test_pack.test_class;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //displayText();
+        for (int k = 0; k < 10; k++) {
+            Log.d("MainActivity", " counter =" + k);
+        }
+    }
+    private void displayText() {
+        test_class tf = new test_class();
+        TextView nt = findViewById(R.id.newtest);
+        nt.setText(tf.getValue());
     }
 }
---
+++ app/src/main/java/bgtu/by/for_github/test_pack/test_class.java
@@ -0,0 +1,7 @@
+package bgtu.by.for_github.test_pack;
+
+public class test_class {
+    public String getValue(){
+        return "Hello from first project";
+    }
+}
